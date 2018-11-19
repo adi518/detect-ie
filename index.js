cde67ad188edbc321e6082499a1c1fcbc5bfc12a
Adi Sahar <adisahar1@hotmail.com>
2018-11-20 00:19:12 +0200

    Remove IE7

--- index.js
+++ index.js
@@ -1,35 +1,22 @@
 /* eslint-disable space-before-function-paren */
 
-// As always with these things, YMMV. ;)
+// As always with these things, YMMV.
 // https://codepen.io/gapcode/pen/vEJNZN
 // https://github.com/stowball/Layout-Engine
 // https://stackoverflow.com/a/14658198/4106263
 // https://en.wikipedia.org/wiki/Internet_Explorer_version_history
 // https://stackoverflow.com/questions/9900311/how-do-i-target-only-internet-explorer-10-for-certain-situations-like-internet-e/14916454#14916454
 
-// Constants
-const IE7_OR_LOWER = '<= 7'
-
-// Implementation
 export function detectIe(options = {}) {
-
-  // Setup Defaults
+  const report = { isDetected: false }
   const defaults = { useUserAgent: false }
-
-  // Setup Options
   options = { ...defaults, ...options }
-
-  // Setup State
-  const state = { isDetected: false }
-
-  // Variables
   let version
 
   // Should use User-Agent based detection? (less reliable, spoofable)  
   if (options.useUserAgent) {
 
     const { userAgent } = window.navigator
-
     const edge = userAgent.indexOf('Edge/')
     const trident = userAgent.indexOf('Trident/')
     const rv = userAgent.indexOf('rv:')
@@ -38,17 +25,17 @@ export function detectIe(options = {}) {
     const isTrident = trident > 0
     const isMsie = msie > 0
 
-    // Detect Edge Version
+    // Detect Edge
     if (isEdge) {
       version = parseInt(userAgent.substring(edge + 5, userAgent.indexOf('.', edge)), 10)
     }
 
-    // Detect Trident (IE 11) Version
+    // Detect IE 11 (Trident)
     else if (isTrident) {
       version = parseInt(userAgent.substring(rv + 3, userAgent.indexOf('.', rv)), 10)
     }
 
-    // Detect IE <= 10 Version
+    // Detect IE <= 10
     else if (isMsie) {
       version = parseInt(userAgent.substring(msie + 5, userAgent.indexOf('.', msie)), 10)
     }
@@ -60,7 +47,6 @@ export function detectIe(options = {}) {
 
     const { style } = document.documentElement
 
-    // Tests
     if ('msScrollLimit' in style || 'behavior' in style) {
 
       // Detect Edge
@@ -87,36 +73,29 @@ export function detectIe(options = {}) {
       else if ('widows' in style) {
         version = 8
       }
-
-      // Detect IE <= 7
-      else {
-        version = IE7_OR_LOWER
-      }
     }
   }
 
   if (version) {
 
-    const versions = [7, 8, 9, 10, 11]
+    const versions = [8, 9, 10, 11]
 
-    // Generate Queries for IE 7-11
+    // Generate Queries for IE 8-11
     const queries = versions.reduce((o, v) => { o[`isIe${v}`] = v === version; return o }, {})
 
-    // Special Queries
+    // Special Queries (Edge)
     const isEdge = version === '>= 12' || version >= 12
-    const isIe7orLower = version === IE7_OR_LOWER || version === 7
     const isBelowEdge = version < 12
 
-    return {
-      ...state,
+    report = {
+      ...report,
       isDetected: true,
       isEdge,
       isBelowEdge,
-      isIe7orLower,
       ...queries,
-      version,
+      version
     }
   }
 
-  return state
+  return report
 }
